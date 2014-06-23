31ea01c1f73035d72327482ec9579d37f8b6a9d5
tanrajp <tanrajp@illycorp.com>
2014-06-23 10:25:23 +0100

    Added Address entity and Address mapping class

--- NHibernateTest/Address.cs
+++ NHibernateTest/Address.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
 
 namespace NHibernateTest
 {
-    class Address
+    public class Address
     {
+        public virtual int Id { get; set; }
+        public virtual string HouseNumber { get; set; }
+        public virtual string StreetName { get; set; }
+        public virtual string PostCode { get; set; }
+        public virtual string City { get; set; }
     }
 }
---
+++ NHibernateTest/AddressMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace NHibernateTest
+{
+    public class AddressMap: ClassMapping<Address>
+    {
+        public AddressMap()
+        {
+            Id(x=>x.Id, m=>m.Generator(Generators.Assigned));
+            Property(x=>x.HouseNumber);
+            Property(x=>x.StreetName);
+            Property(x=>x.PostCode);
+            Property(x=>x.City);
+        }
+    }
+}
