63a8b1d43ddbca8cb4c53eedd7362255e94229d5
tanrajp <tanrajp@illycorp.com>
2014-06-23 13:21:28 +0100

    Added Address repository

---
+++ NHibernateTest/AddressRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTest
+{
+    public class AddressRepository : IAddressRepository
+    {
+        public void Add(Domain.Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Domain.Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Domain.Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Address GetAddressById(Guid addressId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Address GetAddressByStreetName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Address GetAddressForPerson(Domain.Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
